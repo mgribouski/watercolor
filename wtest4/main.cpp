c7dda7ad20a858cfb113a24db64ba667180a9e50
calliope <calliope@mnemosyne.local>
2012-10-07 23:13:39 -0400

    Initial Commit

---
+++ wtest4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wtest4
+//
+//  Created by calliope on 10/7/12.
+//  Copyright (c) 2012 calliope. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
